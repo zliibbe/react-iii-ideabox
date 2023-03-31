f045ef9be985a3838c4dd26e939d07ca003128a8
Zach Liibbe <zliibbe@gmail.com>
2023-03-31 15:15:13 -0600

    Update declaration to ES6 in Form

--- src/Form/Form.js
+++ src/Form/Form.js
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import './Form.css';
 
-function Form(props) {
+const Form = (props) => {
   const [title, setTitle] = useState('')
   const [description, setDescription] = useState('')
 
